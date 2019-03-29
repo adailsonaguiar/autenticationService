20a6d7dc2aa531fdf228bf8c72bb53fb18a4d703
adailsonacj <adailsonacj@live.com>
2019-03-29 16:19:47 -0300

    Loginn e adição de novos usuários Ok

--- src/main/java/com/autentication/controller/UserCon.java
+++ src/main/java/com/autentication/controller/UserCon.java
@@ -7,11 +7,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityManager;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/rest/user")
 public class UserCon {
+
+    private EntityManager em;
     @Autowired
     private UserRepo userRepo;
 
@@ -21,11 +24,15 @@ public class UserCon {
     }
 
     @PostMapping(path = "/add")
-    public @ResponseBody
-    int insertUser(@RequestBody Usuario usuario) {
-        Usuario n = usuario;
-        userRepo.save(n);
-        return n.getId();
+    public ResponseEntity insertUser(@RequestParam String nome, @RequestParam String cpf, @RequestParam String email, @RequestParam String password) {
+        try{
+            Usuario n = new Usuario(nome, cpf, email, password);
+            userRepo.save(n);
+            return ResponseEntity.ok().body(n);
+        }catch(Exception e){
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.BAD_GATEWAY).body("Falha ao salvar usuário, verifique os logs!");
+        }
     }
 
     @PostMapping(path = "login")
@@ -36,14 +43,27 @@ public class UserCon {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("usuário sem permissão!");
     }
 
-    @GetMapping(path = "tras")
-    public ResponseEntity tras() {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("unauthorized");
-    }
-
-    @GetMapping(path = "usuario")
-    public Usuario usuario() {
-        Usuario u = new Usuario("BIBI", "02348203498", "bibi@gmail.com", "123");
-        return u;
-    }
+//    @PostMapping(path = "login")
+//    public Response login(@Context HttpHeaders httpHeaders, @RequestParam String user, @RequestParam String password) {
+//
+//        Autenticator autenticator = Autenticator.getInstance();
+//        String serviceKey = httpHeaders.getHeaderString(Headers.KEY);
+//        try {
+//            String authToken = autenticator.login(user, password, serviceKey);
+//            JsonObjectBuilder jsonObjBuilder = Json.createObjectBuilder();
+//            jsonObjBuilder.add("token", authToken);
+//            JsonObject jsonObj = jsonObjBuilder.build();
+//            return Response.ok(jsonObj.toString()).build();
+//
+//        } catch (final LoginException ex) {
+//            JsonObjectBuilder jsonObjBuilder = Json.createObjectBuilder();
+//            jsonObjBuilder.add("message", "Problem matching key, user and password");
+//            JsonObject jsonObj = jsonObjBuilder.build();
+//
+//            return Response
+//                    .status(Status.UNAUTHORIZED)
+//                    .entity(jsonObj.toString())
+//                    .build();
+//        }
+//    }
 }
