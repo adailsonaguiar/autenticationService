36b9cc411f9c2250047f2738e7ef576a98ea2493
Adailsonacj <adailsonacj@live.com>
2019-03-29 09:51:41 -0300

    retornando Html status

---
+++ src/main/java/com/autentication/controller/Autenticator.java
@@ -0,0 +1,94 @@
+package com.autentication.controller;
+
+import javax.security.auth.login.LoginException;
+import java.security.GeneralSecurityException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.UUID;
+
+public class Autenticator {
+
+    /* Simula uma tabela do banco com os usuarios */
+    private final Map<String, String> users = new HashMap();
+
+    /* Simula uma tabela do banco com as chaves de acesso */
+    private final Map<String, String> keys = new HashMap();
+
+    /* Guarda os tokens gerados em tempo de execução */
+    private final Map<String, String> tokens = new HashMap<>();
+
+    private static Autenticator autenticator = null;
+
+    private Autenticator() {
+        /**
+         * Guarda o usuário e senha dos clientes da API
+         */
+        users.put("walter", "white");
+        users.put("toni", "soprano");
+
+        /**
+         * keys são geradas antecipadamente e disponibilizadas para os clientes da API
+         * Aqui estamos representando as keys para os dois users pré cadastrados.
+         */
+        keys.put("ece40d50-b22b-4b33-a728-343be730d85e", "walter");
+        keys.put("1ee68990-2d56-44a9-ae40-09d0bf074d8d", "toni");
+    }
+
+    public static Autenticator getInstance() {
+        if (Objects.isNull(autenticator)) {
+            autenticator = new Autenticator();
+        }
+        return autenticator;
+    }
+
+    public String login(String user, String password, String key) throws LoginException {
+        if (keys.containsKey(key)) {
+            String userStorage = keys.get(key);
+            if (userStorage.equals(user) && users.containsKey(user)) {
+                String passwordStorage = users.get(user);
+                if (passwordStorage.equals(password)) {
+                    String auth = UUID.randomUUID().toString();
+                    tokens.put(auth, user);
+                    return auth;
+                }
+            }
+        }
+        throw new LoginException("Get Out! you are not welcome!!");
+    }
+
+    public boolean isKeyValid(String key) {
+        return keys.containsKey(key);
+    }
+
+    private boolean isTokenValid(String token) {
+        return tokens.containsKey(token);
+    }
+
+    public boolean isTokenValid(String key, String token) {
+        if (isKeyValid(key)) {
+            String userKey = keys.get(key);
+            if (isTokenValid(token)) {
+                String userToken = tokens.get(token);
+                if (userKey.equals(userToken)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public void logout(String key, String token) throws GeneralSecurityException {
+        if (isKeyValid(key)) {
+            String userKey = keys.get(key);
+            if (isTokenValid(token)) {
+                String userToken = tokens.get(token);
+                if (userToken.equals(userKey)) {
+                    tokens.remove(token);
+                    return;
+                }
+            }
+        }
+        throw new GeneralSecurityException("Invalid key and token.");
+    }
+}
--- src/main/java/com/autentication/controller/UserCon.java
+++ src/main/java/com/autentication/controller/UserCon.java
@@ -3,6 +3,8 @@ package com.autentication.controller;
 import com.autentication.model.Usuario;
 import com.autentication.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,16 +22,28 @@ public class UserCon {
 
     @PostMapping(path = "/add")
     public @ResponseBody
-    int insertUser(@RequestParam String nome, @RequestParam String cpf, @RequestParam String email, @RequestParam String password) {
-        Usuario n = new Usuario(nome, cpf, email, password);
+    int insertUser(@RequestBody Usuario usuario) {
+        Usuario n = usuario;
         userRepo.save(n);
         return n.getId();
     }
 
-    @DeleteMapping("/delete")
-    public @ResponseBody
-    String deletePessoa(@RequestParam int id) {
-        userRepo.deleteById(id);
-        return "Deletado" + id;
+    @PostMapping(path = "login")
+    public ResponseEntity login(@RequestParam String user, @RequestParam String password) {
+        if (user.equals("adailsonacj") && password.equals("123")) {
+            return ResponseEntity.ok().body("usuário autorizado!");
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("usuário sem permissão!");
+    }
+
+    @GetMapping(path = "tras")
+    public ResponseEntity tras() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("unauthorized");
+    }
+
+    @GetMapping(path = "usuario")
+    public Usuario usuario() {
+        Usuario u = new Usuario("BIBI", "02348203498", "bibi@gmail.com", "123");
+        return u;
     }
 }
