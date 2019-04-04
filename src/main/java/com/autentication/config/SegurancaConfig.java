08006ad661d6e61de0d2c34cef709bc6efeb3c35
Adailsonacj <adailsonacj@live.com>
2019-04-04 17:53:26 -0300

    Mudança de configuração do banco

--- src/main/java/com/autentication/config/SegurancaConfig.java
+++ src/main/java/com/autentication/config/SegurancaConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -14,6 +15,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import javax.sql.DataSource;
 
 @Configuration
+@EnableWebSecurity
 public class SegurancaConfig extends WebSecurityConfigurerAdapter {
 
     final
@@ -38,21 +40,10 @@ public class SegurancaConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        /*
-        http.csrf().disable()
-                .authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-                .httpBasic()
-                .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-                */
         http
-                // starts authorizing configurations
                 .antMatcher("/rest/private/**")
                 .authorizeRequests()
                 .anyRequest().authenticated().and()
-
                 .httpBasic().and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 
--- src/main/java/com/autentication/controller/UserCon.java
+++ src/main/java/com/autentication/controller/UserCon.java
@@ -1,6 +1,5 @@
 package com.autentication.controller;
 
-import com.autentication.model.Roles;
 import com.autentication.model.Usuario;
 import com.autentication.repository.RoleRepo;
 import com.autentication.repository.UserRepo;
@@ -10,7 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityManager;
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/rest/public")
@@ -27,51 +25,14 @@ public class UserCon {
     }
 
     @PostMapping(path = "/add")
-    public ResponseEntity insertUser(@RequestParam String nome, @RequestParam String cpf, @RequestParam String email, @RequestParam String password) {
-        try{
+    public ResponseEntity insertUser(@RequestParam(value = "nome") String nome, @RequestParam(value = "cpf") String cpf, @RequestParam(value = "email") String email, @RequestParam(value = "password") String password) {
+        try {
             Usuario n = new Usuario(nome, cpf, email, password, true);
             userRepo.save(n);
-            Roles r = new Roles(n.getUsername(),"ADMIN");
-            roleRepo.save(r);
             return ResponseEntity.ok().body(n);
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.BAD_GATEWAY).body("Falha ao salvar usuário, verifique os logs!");
         }
     }
-
-    /*
-    @PostMapping(path = "login")
-    public ResponseEntity login(@RequestParam String user, @RequestParam String password) {
-        if (user.equals("adailsonacj") && password.equals("123")) {
-            return ResponseEntity.ok().body("usuário autorizado!");
-        }
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("usuário sem permissão!");
-    }
-
-     */
-
-//    @PostMapping(path = "login")
-//    public Response login(@Context HttpHeaders httpHeaders, @RequestParam String user, @RequestParam String password) {
-//
-//        Autenticator autenticator = Autenticator.getInstance();
-//        String serviceKey = httpHeaders.getHeaderString(Headers.KEY);
-//        try {
-//            String authToken = autenticator.login(user, password, serviceKey);
-//            JsonObjectBuilder jsonObjBuilder = Json.createObjectBuilder();
-//            jsonObjBuilder.add("token", authToken);
-//            JsonObject jsonObj = jsonObjBuilder.build();
-//            return Response.ok(jsonObj.toString()).build();
-//
-//        } catch (final LoginException ex) {
-//            JsonObjectBuilder jsonObjBuilder = Json.createObjectBuilder();
-//            jsonObjBuilder.add("message", "Problem matching key, user and password");
-//            JsonObject jsonObj = jsonObjBuilder.build();
-//
-//            return Response
-//                    .status(Status.UNAUTHORIZED)
-//                    .entity(jsonObj.toString())
-//                    .build();
-//        }
-//    }
 }
