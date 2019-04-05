a82b0b897dff95b0b89f09c892306259fedd8e78
Adailsonacj <adailsonacj@live.com>
2019-04-05 09:11:42 -0300

    Configuração do CORS

--- src/main/java/com/autentication/config/SegurancaConfig.java
+++ src/main/java/com/autentication/config/SegurancaConfig.java
@@ -14,13 +14,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.sql.DataSource;
-import java.util.Arrays;
-import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -62,6 +57,7 @@ public class SegurancaConfig extends WebSecurityConfigurerAdapter {
         return NoOpPasswordEncoder.getInstance();
     }
 
+    /*
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurerAdapter() {
@@ -70,5 +66,12 @@ public class SegurancaConfig extends WebSecurityConfigurerAdapter {
                 registry.addMapping("/**").allowedOrigins("*");
             }
         };
+    }
+     */
+    @Bean
+    protected CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
     }
 }
