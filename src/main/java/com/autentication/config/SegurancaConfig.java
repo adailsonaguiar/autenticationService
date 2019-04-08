64bb9dec07863829cae301f1815596419484e668
adailsonacj <adailsonacj@live.com>
2019-04-08 15:21:04 -0300

    alter cors 2

--- src/main/java/com/autentication/config/SegurancaConfig.java
+++ src/main/java/com/autentication/config/SegurancaConfig.java
@@ -4,7 +4,6 @@ package com.autentication.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -62,11 +61,7 @@ public class SegurancaConfig extends WebSecurityConfigurerAdapter {
     @Bean
     protected CorsConfigurationSource corsConfigurationSource() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration cors = new CorsConfiguration();
-        cors.addAllowedMethod(HttpMethod.POST);
-        cors.addAllowedMethod(HttpMethod.GET);
-        cors.applyPermitDefaultValues();
-        source.registerCorsConfiguration("/**", cors);
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
         return source;
     }
 }
