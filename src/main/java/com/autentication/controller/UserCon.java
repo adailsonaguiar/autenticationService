645fc1de450ec92b7847b46075ce3078fefd367a
Adailsonacj <adailsonacj@live.com>
2019-04-08 22:36:55 -0300

    anotação do Cross

--- src/main/java/com/autentication/controller/UserCon.java
+++ src/main/java/com/autentication/controller/UserCon.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityManager;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/public")
 public class UserCon {
